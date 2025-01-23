a47c0fe02cc1c058981d305e7b6bb528f8511225
Sawa <savvalaptev3@gmail.com>
2025-01-23 11:48:11 +0500

    Fix log for big response

--- database/model/apiLog.go
+++ database/model/apiLog.go
@@ -5,12 +5,12 @@ import "time"
 type ApiLog struct {
 	ID            uint   `gorm:"primaryKey" json:"id"`
 	UserID        uint   `gorm:"not null"`
-	RequestMethod string `gorm:"not null"`
+	RequestMethod string `gorm:"not null;type:varchar(255)"`
 	Url           string `gorm:"not null"`
-	RequestBody   string `gorm:"type:text"`
-	RequestHeader string `gorm:"type:text"`
-	Ip            string `gorm:"not null"`
-	ResponseBody  string `gorm:"type:text"`
+	RequestBody   string `gorm:"type:longtext"`
+	RequestHeader string `gorm:"type:longtext"`
+	Ip            string `gorm:"not null;type:varchar(255)"`
+	ResponseBody  string `gorm:"type:longtext"`
 	ResponseCode  uint   `gorm:"not null"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
